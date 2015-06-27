78384b920209c843f007192890b9ebf9181ab0e2
Emeth <seanybob@gmail.com>
2015-06-27 16:14:00 -0500

    more

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,51 @@
+if (!localStorage.getItem("player_id")){
+    localStorage.setItem("player_id", makeid());
+}
+
+function create_room_setup() {
+    $(".full_page").hide();
+    $("#create_room_div").show();
+}
+
+function create_room_submit() {
+
+}
+
+function join_existing_room_setup() {
+    $(".full_page").hide();
+    $("#join_room_div").show();
+}
+
+function join_existing_room_submit() {
+    jQuery.ajax({
+        url: "https://sage-lasertag-api.herokuapp.com/add_player_to_room",
+        dataType: "json",
+        type: "POST",
+        data: {
+            player_id: localStorage.getItem("player_id"),
+            room_code: jQuery("#join_room_room_code").val(),
+        },
+        error: function (e) {
+            console.log("error", e);
+            alert("Error while trying to join existing room");
+        },
+        success:function (data) {
+
+        }
+    });
+}
+
+function makeid()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 8; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
 var app = {
     // Application Constructor
     initialize: function() {
