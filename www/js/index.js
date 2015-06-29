20770a3095f043121b97f0d9160db0e35b126e6d
Emeth <seanybob@gmail.com>
2015-06-29 16:50:26 -0500

    moah

--- www/js/index.js
+++ www/js/index.js
@@ -2,17 +2,17 @@ if (!localStorage.getItem("player_id")){
     // TODO actually transfer this to server-side
     localStorage.setItem("player_id", makeid());
 }
+var poll_match_settimeout;
 
 function poll_match() {
+    clearTimeout(poll_match_settimeout);
     jQuery.ajax({
         url: "http://127.0.0.1:8001/get_match_details",
         dataType: "json",
         type: "POST",
         data: {
             player_id: localStorage.getItem("player_id"),
-            room_code: jQuery("#create_room_room_code").val(),
-            gametype: jQuery("#create_room_gametype").val(),
-            locked_down: jQuery("#create_room_locked_down").val(),
+            room_code: localStorage.getItem("room_code")
         },
         error: function (e) {
             if (e.responseJSON && e.responseJSON.message) {
@@ -25,15 +25,58 @@ function poll_match() {
         },
         success:function (data) {
             $(".waiting_for_match_num_players").text(data.data.number_of_players);
+            var poll_match_delay = 3000;
             if (data.data.match_in_progress) {
-                $("#match_in_progress_gametype").text(data.data.gametype);
+                $(".match_in_progress_gametype").text(data.data.gametype);
+                $(".match_rules_lives_per_spawn").text(data.data.lives_per_spawn);
+                $(".match_rules_length").text(data.data.match_length + " minutes");
+
+                var respawn_timer_text = data.data.respawn_timer + " seconds";
+                if (data.data.respawn_timer == -1) {
+                    respawn_timer_text = "disabled";
+                }
+                $(".match_rules_respawn_timer").text(respawn_timer_text);
                 var scores_htmlz = "";
                 for (var i=0; i<data.data.players.length; i++) {
-                    scores_htmlz += '<tr><th scope="row">'+(i+1)+'</th><td>'+data.data.players[i]['alias']+'</td><td>'+data.data.players[i]['score']+'</td></tr>';
+                    if (data.data.players[i]['player_id'] == localStorage.getItem("player_id")) {
+                        scores_htmlz += '<tr bgcolor="#add8e6">';
+                    }
+                    else {
+                        scores_htmlz += '<tr>';
+                    }
+                    scores_htmlz += '<th scope="row">'+(i+1)+'</th><td>'+data.data.players[i]['alias']+'</td><td>'+data.data.players[i]['score']+'</td></tr>';
                 }
                 $("#match_in_progress_scores").html(scores_htmlz);
+                var countdown_seconds_left = data.data.match_countdown - data.data.match_seconds_elapsed;
+                $("#match_begins_timer").text(countdown_seconds_left);
+                if (data.data.match_seconds_left > 0) {
+                    var match_time_left_seconds = (data.data.match_seconds_left % 60).toString();
+                    if (match_time_left_seconds.length == 1) {
+                        match_time_left_seconds = "0" + match_time_left_seconds;
+                    }
+                    var match_time_left_minutes = (Math.floor(data.data.match_seconds_left / 60)).toString();
+                    $(".match_time_left").text(match_time_left_minutes + ":" + match_time_left_seconds);
+                }
+                else {
+                    $(".match_time_left").text("Game Over!");
+                }
+
+                if (countdown_seconds_left > 0) {
+                    if ($('#match_about_to_start').css('display') == 'none') {
+                        $(".full_page").hide();
+                        $("#match_about_to_start").show();
+                    }
+                    poll_match_delay = 300;
+                }
+                else {
+                    if ($('#match_in_progress').css('display') == 'none') {
+                        $(".full_page").hide();
+                        $("#match_in_progress").show();
+                    }
+                }
             }
-            setTimeout("poll_match()", 3000);
+            clearTimeout(poll_match_settimeout);
+            poll_match_settimeout = setTimeout("poll_match()", poll_match_delay);
         }
     });
 }
@@ -43,14 +86,22 @@ function start_match() {
         player_id: localStorage.getItem("player_id"),
         room_code: localStorage.getItem("room_code"),
         gametype: jQuery("#match_config_gametype").val(),
-        lives_per_player: jQuery("#match_config_lives_per_player").val(),
+        lives_per_spawn: jQuery("#match_config_lives_per_spawn").val(),
+        match_countdown: jQuery("#match_config_countdown").val(),
+        match_length: jQuery("#match_config_length").val(),
         respawn_timer: -1,
     };
     if ($('#match_config_respawn_enabled').prop('checked')) {
         post_data['respawn_timer'] = jQuery("#match_config_respawn_timer").val();
     }
-    if (!parseInt(post_data['lives_per_player'])) {
-        post_data['lives_per_player'] = 1;
+    if (!parseInt(post_data['lives_per_spawn'])) {
+        post_data['lives_per_spawn'] = 1;
+    }
+    if (!parseInt(post_data['match_countdown'])) {
+        post_data['match_countdown'] = 10;
+    }
+    if (!parseInt(post_data['match_length'])) {
+        post_data['match_length'] = 15;
     }
     jQuery.ajax({
         url: "http://127.0.0.1:8001/start_match",
@@ -62,7 +113,7 @@ function start_match() {
             alert("Error while trying to start match!");
         },
         success:function (data) {
-
+            poll_match();
         }
     });
 }
@@ -94,7 +145,6 @@ function create_room_submit() {
                 $("#create_or_join_room_div").show();
             },
             success:function (data) {
-                //TODO set this from the return data instead
                 localStorage.setItem("room_code", data.data.room_code);
                 $("#waiting_for_match_to_start").show();
                 if (data.data.creator_player_id == localStorage.getItem("player_id")) {
@@ -136,7 +186,6 @@ function join_existing_room_submit() {
                 $("#create_or_join_room_div").show();
             },
             success:function (data) {
-                //TODO set this from the return data instead
                 localStorage.setItem("room_code", data.data.room_code);
                 $("#waiting_for_match_to_start").show();
                 if (data.data.creator_player_id == localStorage.getItem("player_id")) {
